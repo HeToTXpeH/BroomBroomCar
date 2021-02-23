b7f1f67addfd95a7b8035786484a947038dbfd12
HeToTXpeH <36816595+HeToTXpeH@users.noreply.github.com>
2021-02-23 19:31:23 +0300

    init

---
+++ Source/BroomBroomCar/BroomBroomCarGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BroomBroomCarGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BROOMBROOMCAR_API ABroomBroomCarGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
